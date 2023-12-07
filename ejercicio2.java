a3e8eca37b703902d76b9bbea65d74df771fcbca
Miguel Ángel Morejón <miguelangelmorejon@gmail.com>
2023-12-07 06:27:53 +0100

    Ejercicios resueltos prueba entregable unidad 3

---
+++ ejercicio2.java
@@ -0,0 +1,32 @@
+
+public class ejercicio2 {
+
+	
+	public static void main(String[] args) {
+		
+		String cadena="           hTtpS://www.Google.eS HTtp://iesAlixaR.oRg hTTps://www.juntadEandalucia.es                  ";
+		String cadena2=cadena.toLowerCase().trim();
+		String subcadena1="https://";
+		String subcadena2="http://";
+		String [] cadenaPartida = cadena2.split(" ");
+		
+		int contadorhttps=0;
+		int contadorhttp=0;
+		
+		for (int i=0;i<cadenaPartida.length;i++) {
+			if (cadenaPartida[i].contains(subcadena1)) {
+				contadorhttps++;
+			}
+			
+			if (cadenaPartida[i].contains(subcadena2)) {
+				contadorhttp++;
+			}		
+			
+		}
+		
+		System.out.println("Número de dominios https:// --> " + contadorhttps);
+		System.out.println("Número de dominios http://  --> "+ contadorhttp);
+
+	}
+
+}
---
+++ ejercicio3.java
@@ -0,0 +1,137 @@
+
+public class ejercicio3 {
+
+	public static void imprimirArrayBI(int [][] numeros) {
+		System.out.println();
+		for (int i=0;i<numeros.length;i++) {
+			System.out.println();
+			for (int j=0;j<numeros.length;j++) {
+				if (numeros[i][j]<10)
+					System.out.print("[ " + numeros[i][j] + "]");
+				else
+					System.out.print("[" + numeros[i][j] + "]");
+			}
+		}
+		
+		
+	}
+	public static void imprimirArrayUNI(int [] numeros) {
+		System.out.println();
+	
+
+		for (int i=0;i<numeros.length;i++) 
+			System.out.print("[" + numeros[i]+ "]");
+			
+		}
+		
+		
+	
+	
+	
+	public static int [] diagonalPrincipal (int [][] numeros) {
+		
+		int [] diagonal = new int[numeros.length];
+		
+		
+		for (int i=0;i<numeros.length;i++) {
+			for (int j=0;j<numeros.length;j++) {
+				if (i==j)
+					diagonal[i]=numeros[i][j];
+			}
+		}
+		
+		
+		
+		return diagonal;
+	}
+	
+	
+public static int [] diagonalSecundaria (int [][] numeros) {
+		
+		int [] diagonal = new int[numeros.length];
+		
+		
+		for (int i=0;i<numeros.length;i++) {
+			for (int j=0;j<numeros.length;j++) {
+				if (j==numeros.length-i-1)
+					diagonal[i]=numeros[i][j];
+			}
+		}
+		
+		
+		
+		return diagonal;
+	}
+
+public static int [] invertirArray(int [] numeros,int inicio, int fin) {
+	
+	
+	if (fin<=inicio)
+		return numeros;
+	else {
+		int aux;
+		aux=numeros[inicio];
+		numeros[inicio]=numeros[fin];
+		numeros[fin]=aux;
+		
+	} 
+			
+	return invertirArray(numeros,inicio+1,fin-1);
+	
+	
+}
+	
+public static void inicializar (int numeros [][]) {
+	for (int i=0;i<numeros.length;i++) {
+		for (int j=0;j<numeros.length;j++) {
+			numeros[i][j]= (int) (Math.random()*25+1);
+		}
+	}
+
+	
+}	
+
+public static void main(String[] args) {
+
+		int numeros [][] = new int [5][5];
+		inicializar(numeros);
+		
+		
+		System.out.println("\nArray principal: ");
+		imprimirArrayBI(numeros);
+		
+		
+		int [] diagonalPrincipal=diagonalPrincipal(numeros);
+		System.out.println("\n\nDiagonal principal: ");
+		imprimirArrayUNI(diagonalPrincipal);
+		int [] diagonalPrincipalInvertida=invertirArray(diagonalPrincipal,0,4);
+		System.out.println("\n\nDiagonal principal invertida: ");
+		imprimirArrayUNI(diagonalPrincipalInvertida);
+		
+		
+		int [] diagonalSecundaria=diagonalSecundaria(numeros);
+		System.out.println("\n\nDiagonal secundaria: ");
+		imprimirArrayUNI(diagonalSecundaria);
+		int [] diagonalSecundariaInvertida;
+		diagonalSecundariaInvertida=invertirArray(diagonalSecundaria,0,4);
+		System.out.println("\n\nDiagonal secundaria invertida: ");
+		imprimirArrayUNI(diagonalSecundariaInvertida);
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
